b35da98d3ecfb3591ebe0f2644dcba6314941fd8
Wiktor <wiktorwszedybyl1@gmail.com>
2026-05-19 22:39:51 +0200

    Translations directory

---
+++ translations/mazemotion_en.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="pl">
+<context>
+    <name>ChartPanel</name>
+    <message>
+        <location filename="../src/chartpanel.cpp" line="29"/>
+        <source>Przyśpieszenie [g]</source>
+        <translation>Acceleration [g]</translation>
+    </message>
+    <message>
+        <location filename="../src/chartpanel.cpp" line="39"/>
+        <source>Prędkość kątowa [°/s]</source>
+        <translation>Angular velocity [°/s]</translation>
+    </message>
+    <message>
+        <location filename="../src/chartpanel.cpp" line="79"/>
+        <source>Akcelerometr</source>
+        <translation>Accelerometer</translation>
+    </message>
+    <message>
+        <location filename="../src/chartpanel.cpp" line="80"/>
+        <source>Żyroskop</source>
+        <translation>Gyroscope</translation>
+    </message>
+</context>
+<context>
+    <name>GameWidget</name>
+    <message>
+        <location filename="../src/gamewidget.cpp" line="108"/>
+        <source>Wygrana!</source>
+        <translation>You win!</translation>
+    </message>
+</context>
+<context>
+    <name>GameWidget3D</name>
+    <message>
+        <location filename="../src/gamewidget3d.cpp" line="346"/>
+        <source>Wygrana!</source>
+        <translation>You win!</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="46"/>
+        <location filename="../src/mainwindow.cpp" line="108"/>
+        <location filename="../src/mainwindow.cpp" line="120"/>
+        <source>Połącz</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="47"/>
+        <location filename="../src/mainwindow.cpp" line="109"/>
+        <source>Generuj labirynt</source>
+        <translation>Generate maze</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="48"/>
+        <source>3D</source>
+        <translation>3D</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="49"/>
+        <location filename="../src/mainwindow.cpp" line="110"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="108"/>
+        <location filename="../src/mainwindow.cpp" line="116"/>
+        <source>Rozłącz</source>
+        <translation>Disconnect</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="110"/>
+        <source>Polski</source>
+        <translation>Polski</translation>
+    </message>
+</context>
+</TS>
